10507258afef1a282919d369a4b22caae6d838c9
yanan <iyanan@hotmail.com>
2021-01-12 09:38:11 +0800

    train my neural network

--- src/imgClassifier/sketch.js
+++ src/imgClassifier/sketch.js
@@ -1,29 +1,42 @@
-// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
 let classifier;
 
-// A variable to hold the image we want to classify
 let img;
 
 function preload() {
-  classifier = ml5.imageClassifier('MobileNet');
-  img = loadImage('images/bird3.jpg');
+  classifier = ml5.imageClassifier('MobileNet', (error, model) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Model ready!')
+    }
+  });
+  img = loadImage('images/bird.jpg');
 }
 
 function setup() {
   createCanvas(400, 400);
-  classifier.classify(img, gotResult);
+  classifier.classify(img, (error, results) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      createDiv(`Label: ${results[0].label}`);
+      createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
+    }
+  });
   image(img, 0, 0);
 }
 
-// A function to run when we get any errors and the results
-function gotResult(error, results) {
-  // Display error in the console
-  if (error) {
-    console.error(error);
-  } else {
-    // The results are in an array ordered by confidence.
-    console.log(results);
-    createDiv(`Label: ${results[0].label}`);
-    createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
-  }
-}
+// using promise and arrow function
+// function setup() {
+//   let img = loadImage('images/bird.jpg')
+//   ml5.imageClassifier('MobileNet').then(classifier => classifier.classify(img)).then((error, results) => {
+//     if (error) {
+//       console.log(error)
+//     } else {
+//       console.log(results);
+//       createDiv(`Label: ${results[0].label}`);
+//       createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
+//     }
+//   })
+// }
---
+++ src/neuralNetwork/script.js
@@ -0,0 +1,57 @@
+
+const options = {
+  dataUrl: './data/colorData.json',
+  inputs: ['r', 'g', 'b'],
+  outputs: ['color'],
+  task: 'classification',
+  debug: true
+}
+
+
+// 初始化 神经网络
+const nn = ml5.neuralNetwork(options, dataLoaded);
+
+
+function dataLoaded() {
+  // 数据加载后 规格化数据 并 训练模型
+  console.log('data loaded!')
+  nn.normalizeData();
+  console.log('done normalization!')
+  trainModel();
+}
+
+function trainModel() {
+  const trainingOptions = {
+    epochs: 32,
+    batchSize: 12
+  }
+  console.log('training starts...')
+  nn.train(trainingOptions, whileTraining, doneTraining)
+}
+
+function whileTraining(loss) {
+  if (loss) {
+    console.log(loss)
+  }
+}
+
+function doneTraining() {
+  console.log('training complete!')
+  doClassify()
+}
+
+// 根据模型与输入 做出分类判断
+function doClassify() {
+  const input = {
+    r: 255,
+    g: 122,
+    b: 250
+  }
+  nn.classify(input, (error, results) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(results)
+  })
+}
